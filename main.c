99a304d499afba1d3f715ab136d54ba17a6fe791
next <next@qq.com>
2023-02-18 23:16:22 +0800

    添加 develop.h 中新增 add function

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "develop.h"
 
+extern int max(int x, int y);
 int main(int argc, char const *argv[])
 {
     printf("hello world\n");
+    printf("a + b = %d\n", add(100, 200));
+    system("pause");
     return 0;
 }
